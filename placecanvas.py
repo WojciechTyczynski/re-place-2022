a4980149cf63cd464161fba17be6a93808e756cd
koldbrandt <mailtilmb@gmail.com>
2022-11-28 13:42:54 +0100

    added some stuff

--- placecanvas.py
+++ placecanvas.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
-class PlaceCanvas:
+class placecanvas:
     def __init__(self, width, height):
         self.canvas = Image.new('RGB', (width, height), color = 'white')
 
 
-    def update_pixel(self, x,y,color):
+    def update_pixel_hex(self, x,y,color):
         """
         Sets the specific x,y coordinate in the canvas to the given hex color.
 
@@ -16,14 +16,32 @@ class PlaceCanvas:
         rgb_value = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
         self.canvas.putpixel((x,y), rgb_value)
 
-    def save_canvas(self, path = "images"):
-        if(os.path.exists(f'{path}/images.png')):
+    def update_pixel_int(self, x,y,color):
+        """
+        Sets the specific x,y coordinate in the canvas to the given hex color.
+
+        example:
+        update_pixel(10,10, 8318294)
+        """
+        self.canvas.putpixel((x,y), self.getRGBfromI(color))
+
+    def save_canvas(self, path = "images/"):
+        name = ''
+        if(os.path.exists(f'{path}images.png')):
             i = 0 
-            while(os.path.exists(f'{path}/images {i}.png')):
+            while(os.path.exists(f'{path}images {i}.png')):
                 i += 1
-            self.canvas.save(f'{path}/images {i}.png')
+            self.canvas.save(f'{path}images {i}.png')
+            return f'images {i}.png'
         else:
-            self.canvas.save(f'{path}/images.png')
+            self.canvas.save(f'{path}images.png')
+            return 'images.png'
 
     def get_image(self):
         return self.canvas
+
+    def getRGBfromI(self, RGBint):
+        blue =  RGBint & 255
+        green = (RGBint >> 8) & 255
+        red =   (RGBint >> 16) & 255
+        return red, green, blue
